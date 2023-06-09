693ba25bd584c98816aa6bf43cfea05448c1cd94
mehdi <msalehi20@gmail.com>
2023-06-10 01:06:23 +0330

    Revert dl_matrix script

--- fusion/scripts/dl_matrix.py
+++ fusion/scripts/dl_matrix.py
@@ -5,15 +5,13 @@ import os
 
 def dl_save_list(matrix_directory, matrix_list_path):
     # specify what matrices should be downloaded
-    result = search(rowbounds=(10000, 100000), colbounds=(10000, 100000), limit=1000, kind='Directed Graph')
-    # result.download(extract=True, destpath=matrix_directory)
+    result = search(nzbounds=(100000, 1000000), isspd=True, limit=10000000000, dtype='real')
+    result.download(extract=True, destpath=matrix_directory)
     # generate the list of downloaded matrices
     matrix_list = []
     for matrix in result:
         # join two paths to get the full path of the matrix and append it to the matrix list
-        if 'random' not in matrix.kind and 'sequence' not in matrix.kind and 'weighted':
-            matrix_list.append(os.path.join(matrix.name, matrix.name + '.mtx'))
-            matrix.download(extract=True, destpath=matrix_directory)
+        matrix_list.append(os.path.join(matrix.name, matrix.name + '.mtx'))
     # write the matrix list to a txt file in the matrix_list directory
     with open(matrix_list_path, 'w') as f:
         for item in matrix_list:
@@ -22,3 +20,4 @@ def dl_save_list(matrix_directory, matrix_list_path):
 
 if __name__ == '__main__':
     dl_save_list(sys.argv[1], sys.argv[2])
+
