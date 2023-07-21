7685fbda71cd92dcce5c52b3d00253954ebdbdde
Kazem <kazem.cheshmi@gmail.com>
2023-07-20 23:56:14 -0400

    updated banded gen

--- fusion/scripts/gen_matrix.py
+++ fusion/scripts/gen_matrix.py
@@ -19,9 +19,24 @@ def generate_banded_matrix(n):
                             shape=(n, n))
 
 
+def generate_banded_matrix_efficient(n, bandwidth):
+    triplet_list = []
+    half = (bandwidth-1)/2
+    for i in range(n):
+        min_idx = int(max(0, i-half))
+        max_idx = int(min(n, i+half+1))
+        for j in range(min_idx, max_idx):
+            triplet_list.append((i, j, 1))
+    return sp.coo_matrix((np.ones(len(triplet_list)), (np.array(triplet_list)[:, 0], np.array(triplet_list)[:, 1])),
+                            shape=(n, n))
+
+
+
+
+
 # main function
 if __name__ == '__main__':
-    mat = generate_banded_matrix(int(sys.argv[1]))
+    mat = generate_banded_matrix_efficient(int(sys.argv[1]), int(sys.argv[2]))
     # save mat as mtx file
-    sio.mmwrite(sys.argv[2], mat)
+    sio.mmwrite(sys.argv[3], mat)
 
