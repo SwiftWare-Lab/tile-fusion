824c9d7c9a0a9e95c440b052657e05bb4432bc54
mehdi <msalehi20@gmail.com>
2023-09-18 08:07:48 -0400

    Fix some issues of prototype

--- modeling/SAGEGraph-GCN/graphsage_prototype.py
+++ modeling/SAGEGraph-GCN/graphsage_prototype.py
@@ -1,46 +1,42 @@
 import torch
 import random
 
-def layer2_forward(nodes ,adj_list, input_channel, output_channel):
-    
-    for x in nodes:
-        layer1_nodes = adj_list[x]
-        layer1_nodes.append(x)
-        layer1_features = layer1_forward(layer1_nodes)
-    aggregated_features = aggregate_features(layer1_features[:-1])
-
-def layer1_forward(nodes, adj_list, features, output_dim):
-    for x in nodes:
-        aggregated_features = aggregate_features(adj_list[x])
-    return    
-def aggregate_features(neighbors):
-    return
 
 class GraphSage(torch.nn.Module):
 
-    def __init__(self, adj_mtx, features, feat_dim, out_dim):
+    def __init__(self, adj_mtx, features, feat_dim, hidden_dim, out_dim):
+        super(GraphSage, self).__init__()
         self.adj_mtx = adj_mtx
         self.features = features
         self.feat_dim = feat_dim
         self.out_dim = out_dim
-
+        self.w1 = torch.nn.Parameter(torch.randn(feat_dim, hidden_dim))
+        self.w2 = torch.nn.Parameter(torch.randn(hidden_dim, out_dim))
 
     def forward(self, nodes):
         return
 
+    # layer2: Compute embeddings by aggregating result of layer 1
     def layer2_forward(self, nodes):
-        for x in nodes:
-            layer1_nodes = self.adj_mtx[x].nonzeros()
-            layer1_nodes.append(x)
-            layer1_embeds = layer1_forward(layer1_nodes)
-            aggregated_features = aggregate_features(len(self.adj_mtx[x].nonzeros()),None,layer1_embeds[:-1])
-            layer2_features = concat(layer1_embeds[-1],aggregated_features)
-    
-    def layer1_forward(self,nodes):
-        for x in nodes:
-            neigh_feat = self.features[self.adj_mtx[x].nonzeros()]           
-            aggregated_features = aggregate_features(len(self.adj_mtx[x].nonzeros()), None, neigh_feat)
-            layer1_features = concat(features[x], aggregated_features)
-            
+        layer2_out = []
+        for i in range(len(nodes)):
+            node = nodes[i]
+            layer1_nodes = self.adj_mtx[node].nonzeros()
+            layer1_nodes.extend(node)
+            layer1_embeds = self.layer1_forward(layer1_nodes)
+            aggregated_features = self.aggregate_features(len(self.adj_mtx[node].nonzeros()), layer1_embeds[:-1])
+            layer2_out.append(torch.cat([layer1_embeds[-1], aggregated_features], dim=1))
+        return torch.stack(layer2_out).matmul(self.w2)
+
+    # layer1: Compute embeddings by aggregating neighbors in depth 1
+    def layer1_forward(self, nodes):
+        layer1_feat = []
+        for i in range(len(nodes)):
+            node = nodes[i]
+            neigh_feat = self.features[self.adj_mtx[node].nonzeros()]
+            aggregated_features = self.aggregate_features(len(self.adj_mtx[node].nonzeros()), neigh_feat)
+            layer1_feat.append(torch.cat([self.features[node], aggregated_features], dim=1))
+        return torch.stack(layer1_feat).matmul(self.w1)
+
     def aggregate_features(self, neighbors_num, features):
-        return features.sum(2).div(neighbors_num)
+        return features.sum(0).div(neighbors_num)
