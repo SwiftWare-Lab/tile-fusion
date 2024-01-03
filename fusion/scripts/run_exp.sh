6477cb6b08eaae6f4f5042362a0659c0e057b275
Mohammad Mahdi Salehi Dezfuli <salehm32@mcmaster.ca>
2024-01-03 16:07:34 -0500

    Update for fused-ratio experiment

--- fusion/scripts/run_exp.sh
+++ fusion/scripts/run_exp.sh
@@ -59,21 +59,21 @@ if [ "$TUNED" ==  3 ]; then
   while read line; do
     mat=$line
     # shellcheck disable=SC2039
-    for w in {100,1000,5000,10000,500000}; do
+#    for w in {100,1000,5000,10000,500000}; do
       k=4
-      for ntile in {4,8,16,32,64,128,256,512}; do
+      for ntile in {4,8,16,32,64,128,256,512,1024,2048,4096,8192}; do
 #        if [ $ntile -gt $BCOL ]; then
 #          continue
 #        fi
       echo "for $line $BCOL $w $ntile"
       if [ $header -eq 1 ]; then
-        $BINLIB  -sm $PATHMAIN/$mat -nt $THRDS -ah -bc $BCOL -ip $w -tm $ntile -tn $ntile > $LOGS/spmv_spmv_$BCOL.csv
+        $BINLIB  -sm $PATHMAIN/$mat -nt $THRDS -ah -bc $BCOL -ip $ntile -tm $ntile -tn $ntile > $LOGS/spmv_spmv_$BCOL.csv
         header=0
       else
-        $BINLIB  -sm $PATHMAIN/$mat -nt $THRDS -bc $BCOL -ip $w -tm $ntile -tn $ntile >> $LOGS/spmv_spmv_$BCOL.csv
+        $BINLIB  -sm $PATHMAIN/$mat -nt $THRDS -bc $BCOL -ip $ntile -tm $ntile -tn $ntile >> $LOGS/spmv_spmv_$BCOL.csv
       fi
       done
-    done
+#    done
   done < ${MATLIST}
 fi
 
