5dbea330c22399477049dba74893831e48c1076c
Mohammad Mahdi Salehi Dezfuli <salehm32@mcmaster.ca>
2024-01-20 10:11:12 -0500

    Add jacobi fused

--- fusion/jacobi/jacobi_demo.cpp
+++ fusion/jacobi/jacobi_demo.cpp
@@ -60,14 +60,37 @@ int main(const int argc, const char *argv[]) {
   unfused->run();
   //unfused->OutTensor->printDx();
   inJacobi->CorrectSol = std::copy(
-      unfused->OutTensor->Xx,
-      unfused->OutTensor->Xx + unfused->OutTensor->M * unfused->OutTensor->K,
+      unfused->OutTensor->Xx2,
+      unfused->OutTensor->Xx2 + unfused->OutTensor->M * unfused->OutTensor->K,
       inJacobi->CorrectSol);
   inJacobi->IsSolProvided = false;
   auto headerStat = unfused->printStatsHeader();
   auto baselineStat = unfused->printStats();
   delete unfused;
   delete stats;
+
+  stats = new swiftware::benchmark::Stats("Jacobi_BiIterationFused_Demo", "Jacobi CSR", 7,
+                                          tp._matrix_name, numThread);
+  stats->OtherStats["PackingType"] = {Interleaved};
+  auto *fused = new JacobiCSRFused(inJacobi, stats, sp);
+  fused->run();
+  auto fusedStat = fused->printStats();
+  delete fused;
+  delete stats;
+
+  auto csvInfo = sp.print_csv(true);
+  std::string spHeader = std::get<0>(csvInfo);
+  std::string spStat = std::get<1>(csvInfo);
+
+  auto tpCsv = tp.print_csv(true);
+  std::string tpHeader = std::get<0>(tpCsv);
+  std::string tpStat = std::get<1>(tpCsv);
+
+  if(tp.print_header){
+    std::cout << headerStat+spHeader+tpHeader << std::endl;
+  }
+  std::cout << baselineStat+spStat+tpStat << std::endl;
+  std::cout << fusedStat+spStat+tpStat << std::endl;
 
   delete aCSC;
   delete aCSCFull;
