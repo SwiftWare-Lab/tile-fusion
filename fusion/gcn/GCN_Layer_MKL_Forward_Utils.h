3df83efd727c7610b8776273cab98f0229c4a254
Mohammad Mahdi Salehi Dezfuli <salehm32@mcmaster.ca>
2024-03-25 12:07:19 -0400

    Use no transpose in MKL GeMM

--- fusion/gcn/GCN_Layer_MKL_Forward_Utils.h
+++ fusion/gcn/GCN_Layer_MKL_Forward_Utils.h
@@ -126,9 +126,9 @@ void forwardForOneLayerFusedParallelSeparatedVectorizedSP(
         int iL1 = Partition[kBeginL1];
         int tileSize = kEndL1 - kBeginL1;
         cblas_sgemm(
-            CblasRowMajor, CblasNoTrans, CblasTrans, tileSize, OutputChannelDim,
+            CblasRowMajor, CblasNoTrans, CblasNoTrans, tileSize, OutputChannelDim,
             InputChannelDim, 1., Features + iL1 * InputChannelDim,
-            InputChannelDim, Weight, InputChannelDim, 0.,
+            InputChannelDim, Weight, OutputChannelDim, 0.,
             IntermediateResult + iL1 * OutputChannelDim, OutputChannelDim);
         int kEndL2 = MixPtr[j1 * numKernels + 1];
         for (int k1 = kEndL1; k1 < kEndL2; ++k1) {
@@ -548,8 +548,8 @@ void forwardForOneLayerWithMKLGeMMAndMKLSpMMSP(int NumOfNodes,
                                              float *Output, float *IntermediateResult) {
   matrix_descr d;
   d.type = SPARSE_MATRIX_TYPE_GENERAL;
-  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans, NumOfNodes, OutDim,
-              FeatDim, 1., Features, FeatDim, Weight, FeatDim, 0.,
+  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, NumOfNodes, OutDim,
+              FeatDim, 1., Features, FeatDim, Weight, OutDim, 0.,
               IntermediateResult,
               OutDim);
   mkl_sparse_s_mm(SPARSE_OPERATION_NON_TRANSPOSE, 1, AdjMatrix, d,
@@ -651,8 +651,8 @@ void forwardForOneLayerWithMKLGeMMAndSpMMSPVectorized(int NumOfNodes, int *Ap, i
                                             int OutDim, float *Output, float *IntermediateResult,int NumThreads) {
   matrix_descr d;
   d.type = SPARSE_MATRIX_TYPE_GENERAL;
-  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans, NumOfNodes, OutDim,
-              FeatDim, 1., Features, FeatDim, Weight, FeatDim, 0.,
+  cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, NumOfNodes, OutDim,
+              FeatDim, 1., Features, FeatDim, Weight, OutDim, 0.,
               IntermediateResult,
               OutDim);
 #pragma omp parallel num_threads(NumThreads)
