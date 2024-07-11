c4d2495d06636aa17b1137926c6d7a708a2a9d64
Mohammad Mahdi Salehi Dezfuli <salehm32@mcmaster.ca>
2024-07-11 18:53:37 -0400

    Fix script for e2e

--- binding-project/gcn-e2e-training/FusedGCNModule.py
+++ binding-project/gcn-e2e-training/FusedGCNModule.py
@@ -6,8 +6,8 @@ import numpy as np
 class FusedGCN(torch.nn.Module):
     def __init__(self, feat_dim, embed_dim , num_classes, adj, num_threads):
         super(FusedGCN, self).__init__()
-        self.conv1 = FusedGCNLayer(feat_dim, embed_dim, 32, adj, num_threads)
-        self.conv2 = FusedGCNLayer(embed_dim, num_classes,  32, adj, num_threads)
+        self.conv1 = FusedGCNLayer(feat_dim, embed_dim, 128, adj, num_threads)
+        self.conv2 = FusedGCNLayer(embed_dim, num_classes,  128, adj, num_threads)
 
     def forward(self, x):
         x = self.conv1(x)
--- binding-project/run_binding_benchmark.sh
+++ binding-project/run_binding_benchmark.sh
@@ -56,14 +56,18 @@ cd ..
 
 HEADER=1
 # shellcheck disable=SC2162
-while read line; do
-    mat="$DATA_FOLDER/$line"
-    if [ $HEADER -eq 1 ]; then
-      python binding_benchmark.py $mat $BCOL $THRD $HEADER > ./build/logs/benchmark_results.csv
-      HEADER=0
-    else
-      python binding_benchmark.py $mat $BCOL $THRD $HEADER >> ./build/logs/benchmark_results.csv
-    fi
-done < "${MAT_LIST}"
+#while read line; do
+#    mat="$DATA_FOLDER/$line"
+#    if [ $HEADER -eq 1 ]; then
+#      python binding_benchmark.py $mat $BCOL $THRD $HEADER > ./build/logs/benchmark_results.csv
+#      HEADER=0
+#    else
+#      python binding_benchmark.py $mat $BCOL $THRD $HEADER >> ./build/logs/benchmark_results.csv
+#    fi
+#done < "${MAT_LIST}"
 
+echo 'impl,matrix,time' > ./build/logs/e2e_$BCOL.csv
+
+python gcn-e2e-training/fused-gcn-training.py --threads $THRD --hidden_channels $BCOL >> ./build/logs/e2e_$BCOL.csv
+python gcn-e2e-training/gcn-pyg.py --threads $THRD --hidden_channels $BCOL >> ./build/logs/e2e_$BCOL.csv
 deactivate
