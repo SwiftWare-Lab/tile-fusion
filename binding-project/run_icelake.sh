c71c923b23b522beae5c3869a819a5a780ce68f6
Chameleon Cloud User <mmsalehid>
2024-08-02 21:53:10 +0000

    Fix run scripts

---
+++ binding-project/run_icelake.sh
@@ -0,0 +1,3 @@
+bash run_binding_benchmark.sh  -t 80 -c 32 -m ~/data/e2e-graphs/ -v ../venv/
+bash run_binding_benchmark.sh  -t 80 -c 64 -m ~/data/e2e-graphs/ -v ../venv/
+bash run_binding_benchmark.sh  -t 80 -c 128 -m ~/data/e2e-graphs/ -v ../venv/
